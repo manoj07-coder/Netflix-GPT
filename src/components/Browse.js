53b24e132e1b62614a92b84a25d71e6d88887424
Manoj S G <manojgowdacs35@gmail.com>
2025-08-18 14:24:22 +0530

    setup movie store

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,25 @@
+import { useEffect } from "react";
+import { OPTIONS } from "../utils/constants";
 import Header from "./Header";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 const Browse = () => {
+  const dispatch = useDispatch();
+
+  const nowPlayingMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?page=1",
+      OPTIONS
+    );
+    const json = await data.json();
+    dispatch(addNowPlayingMovies(json.results));
+  };
+
+  useEffect(() => {
+    nowPlayingMovies();
+  }, []);
+
   return (
     <div className="">
       <Header />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,12 @@ export const LOGO_URL =
 
 export const USER_AVATAR =
   "https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.webp";
+
+export const OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMzMwYWU4YzcwNjg2NzhkYTVjOGJiMWNlMzM1YWI2ZCIsIm5iZiI6MTc1NTQ0MDcwMi4xMjcsInN1YiI6IjY4YTFlNjNlNGIyNmE0YWJlNmVkMTkwMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.x-juFJFUy_up8qFJldSErbLhN4EhLl-ciozK_sizBz8",
+  },
+};
